a107db9b14add7f4882197f9bdc7e53542484f9d
hyper-ub <70685160+hyper-ub@users.noreply.github.com>
2020-09-30 16:09:22 +0530

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -39,7 +39,9 @@ PM_START_TEXT = """
 ℍ𝕀𝕋 ℍ𝔼𝕃ℙ 𝔽𝕆ℝ ℂ𝕆𝕄𝕄𝔸ℕ𝔻 /help
 
 """
-SABER_IMG = "https://telegra.ph/file/a306066dac39e1ec64663.jpg"
+
+
+BOT_IMG = "https://telegra.ph/file/a306066dac39e1ec64663.jpg"
 
 HELP_STRINGS = """
 
