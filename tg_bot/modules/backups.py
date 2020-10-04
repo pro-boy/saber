2713848df844ff5d5beed41021069a08de0db106
hyper-ub <70685160+hyper-ub@users.noreply.github.com>
2020-10-04 07:38:38 +0530

    Update backups.py

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -67,7 +67,7 @@ def export_data(bot: Bot, update: Update):
 
 __help__ = """
 *Admin only:*
- - /import: reply to a group butler backup file to import as much as possible, making the transfer super simple! Note \
+ - /import: reply to a saber backup file to import as much as possible, making the transfer super simple! Note \
 that files/photos can't be imported due to telegram restrictions.
  - /export: !!! This isn't a command yet, but should be coming soon!
 """
