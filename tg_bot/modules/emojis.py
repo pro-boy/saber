7d9f1cac54af188364ab8327512c2692bdcc837a
hyper-ub <70685160+hyper-ub@users.noreply.github.com>
2020-10-04 08:18:33 +0530

    Update emojis.py

--- tg_bot/modules/emojis.py
+++ tg_bot/modules/emojis.py
@@ -310,7 +310,7 @@ def hack(bot: Bot, update: Update):
     for x in range(EDIT_TIMES):
         msg.edit_text(hack_you[x%5])
         time.sleep(EDIT_SLEEP)
-    msg.edit_text('successful hacked')
+    msg.edit_text('successful hacked,pay 200$ to remove this hack')
 
 
 
